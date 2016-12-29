a914a6dbef59cc422b0b6e7d89ef846e348713fc
Dan Berindei <dan@infinispan.org>
2016-12-29 10:20:07 +0200

    Test with prebuilt versions

--- embedded/runTest.sh
+++ embedded/runTest.sh
@@ -26,14 +26,18 @@ LOG_MAIN=1; LOG_HICCUP=0; LOG_PERFNORM=0; LOG_GC=0; LOG_JITWATCH=0; LOG_PERFASM=
 #LOG_MAIN=0; LOG_HICCUP=0; LOG_PERFNORM=0; LOG_GC=0; LOG_JITWATCH=1; LOG_PERFASM=0; LOG_JFR=0
 #LOG_MAIN=0; LOG_HICCUP=0; LOG_PERFNORM=0; LOG_GC=0; LOG_JITWATCH=0; LOG_PERFASM=1; LOG_JFR=0
 
-INFINISPAN_HOME=$HOME/Work/infinispan
-#INFINISPAN_HOME=$HOME/Work/jdg
+#INFINISPAN_PREBUILT_VERSIONS=""
+INFINISPAN_PREBUILT_VERSIONS="8.3.0.Final-redhat-1"
 
-INFINISPAN_COMMITS="t_async_api_change_experiments"
-#INFINISPAN_COMMITS="master_before_triangle"
-#INFINISPAN_COMMITS="JDG_7.0.0.GA jdg-7.1.x"
-#INFINISPAN_COMMITS="JDG-670/replication_read_performance jdg-7.1.x JDG_7.1.0.DR2"
+WORK_DIR=$HOME/Work
+INFINISPAN_HOME=$WORK_DIR/infinispan
+#INFINISPAN_HOME=$WORK_DIR/jdg
 
+INFINISPAN_COMMITS=""
+#INFINISPAN_COMMITS="t_async_api_change_experiments"
+#INFINISPAN_COMMITS="jdg-7.1.x"
+
+FORCE_JGROUPS_VERSION=""
 #FORCE_JGROUPS_VERSION="3.6.10.Final"
 
 INFINISPAN_CONFIG="../config/infinispan-synctx.xml"
@@ -41,9 +45,9 @@ INFINISPAN_CONFIG="../config/infinispan-synctx.xml"
 JGROUPS_CONFIG="default-configs/default-jgroups-udp.xml"
 
 COMMON_OPTS="-XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -Xmx4g -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -Xloggc:gc.log -Djava.net.preferIPv4Stack=true -Dorg.jboss.logging.provider=log4j2"
-COMMON_OPTS="$COMMON_OPTS -Dlog4j.configurationFile=file:///home/dan/Work/infinispan-microbenchmarks/config/log4j2.xml"
+COMMON_OPTS="$COMMON_OPTS -Dlog4j.configurationFile=file:///$WORK_DIR/infinispan-microbenchmarks/config/log4j2.xml"
 #COMMON_OPTS="-XX:MaxInlineLevel=20"  #inlining helps a bit the async interceptors
-#THROUGHPUT_OPTS="-javaagent:/home/dan/Work/jHiccup/jHiccup.jar=\"-d $WARMUP_SECONDS -i 1000 -l hiccup.hlog\" -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime"
+#THROUGHPUT_OPTS="-javaagent:/$WORK_DIR/jHiccup/jHiccup.jar=\"-d $WARMUP_SECONDS -i 1000 -l hiccup.hlog\" -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime"
 THROUGHPUT_OPTS="-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime"
 PERFASM_OPTS="-XX:PrintAssemblyOptions=intel"
 JITWATCH_OPTS="-XX:+TraceClassLoading -XX:+LogCompilation -XX:+PrintInlining -XX:+PrintAssembly -XX:PrintAssemblyOptions=intel"
@@ -78,7 +82,7 @@ mvn clean package -Dversion.infinispan=$INFINISPAN_VERSION -Dversion.jgroups=$JG
 #done
 #echo $i >.current-index
 
-BASENAME=$PREFIX-$(date +%Y%m%d-%H%M)-${COMMIT//[^a-zA-Z0-9-_.]/_}
+BASENAME=$PREFIX-$(date +%Y%m%d-%H%M)-${SUFFIX//[^a-zA-Z0-9-_.]/_}
 log_version > $BASENAME.log
 echo Results will be in $BASENAME.log
 
@@ -142,7 +146,8 @@ tail -1 $BASENAME.log
 
 # main
 for COMMIT in $INFINISPAN_COMMITS; do
-  BUILD_DIR=/home/dan/Work/tmpbuild/infinispan-microbenchmarks
+  SUFFIX=$COMMIT
+  BUILD_DIR=$WORK_DIR/tmpbuild/infinispan-microbenchmarks
   rm -rf $BUILD_DIR
   git clone -s $INFINISPAN_HOME $BUILD_DIR
   pushd $BUILD_DIR
@@ -157,6 +162,19 @@ for COMMIT in $INFINISPAN_COMMITS; do
   LAST_COMMITS=$(git log --pretty=format:"%H %s" -3 | git name-rev --stdin)
   popd
 
-  run_build || echo Failed to run test for $tag
+  run_build || echo "Failed to run test for $COMMIT"
+  rm -rf $BUILD_DIR
+done
+
+for INFINISPAN_VERSION in $INFINISPAN_PREBUILT_VERSIONS; do
+  SUFFIX=$INFINISPAN_VERSION
+  if [ -z $FORCE_JGROUPS_VERSION ]; then
+    JGROUPS_VERSION=$(cat ~/.m2/repository/org/infinispan/infinispan-bom/$INFINISPAN_VERSION/infinispan-bom-$INFINISPAN_VERSION.pom | perl -ne 'if (/<version.jgroups>(.*)<\/version.jgroups>/) { print "$1\n" }')
+  else
+    JGROUPS_VERSION=$FORCE_JGROUPS_VERSION
+  fi
+  LAST_COMMITS="Prebuilt $INFINISPAN_VERSION"
+
+  run_build || echo "Failed to run test for $INFINISPAN_VERSION"
   rm -rf $BUILD_DIR
 done
