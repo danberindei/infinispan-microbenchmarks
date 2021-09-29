ed154c8447d03651442f067d4704b7944e55f732
Dan Berindei <dan@infinispan.org>
2021-09-29 13:14:30 +0300

    Run embedded tests with new config

--- embedded/runTest.sh
+++ embedded/runTest.sh
@@ -8,23 +8,24 @@ BENCHMARK_MODE=thrpt
 BENCHMARK_TIME_UNITS=s
 #BENCHMARK_MODE=sample
 #BENCHMARK_TIME_UNITS=ns
-CLUSTER_SIZE=8
-NUM_THREADS=100
+CORES=6-11
+CLUSTER_SIZE=4
+NUM_THREADS=16
 KEY_SIZE=20
 VALUE_SIZE=1000
-NUM_KEYS=1000
-FILL_RATIO=1.0
-WARMUP_SECONDS=60
-TEST_SECONDS=45
+NUM_KEYS=100000
+FILL_RATIO=0.7
+WARMUP_SECONDS=50
+TEST_SECONDS=50
 
 #PREFIX=All ; TEST=".*CacheBenchmark.*"
 #PREFIX=NonTxRead ; TEST="(Local|Repl|RandomDist).*testGet"
 #PREFIX=ETxRead ; TEST="(Repl|RandomDist).*testTxGet"
 #PREFIX=LocalRead ; TEST="Local.*testGet"
-PREFIX=LocalWrite ; TEST="Local.*testPut"
+#PREFIX=LocalWrite ; TEST="Local.*testPut"
 #PREFIX=NonTxReplRead ; TEST="Repl.*testGet"
 #PREFIX=ReplWrite ; TEST="Repl.*testPut"
-#PREFIX=ETxReplRead ; TEST="Repl.*testTxGet"
+PREFIX=ETxReplRead ; TEST="Repl.*testTxGet"
 #PREFIX=ETxReplWrite ; TEST="Repl.*testTxPut"
 #PREFIX=Repl ; TEST="Repl.*"
 #PREFIX=PrimaryDistRead ; TEST="PrimaryDist.*testGet"
@@ -34,60 +35,69 @@ PREFIX=LocalWrite ; TEST="Local.*testPut"
 #PREFIX=RandomDistTxRead ; TEST="RandomDist.*testTxGet"
 #PREFIX=MH3 ; TEST="MurmurHash3Benchmark.testHash"
 
-LOG_MAIN=0; LOG_HICCUP=0; LOG_PERFNORM=0; LOG_GC=0; LOG_JITWATCH=0; LOG_PERFASM=0; LOG_JFR=0
+LOG_MAIN=0; LOG_HICCUP=0; LOG_PERFNORM=0; LOG_GC=0; LOG_JITWATCH=0; LOG_PERFASM=0; LOG_JFR=0; LOG_ASYNC=0
 LOG_MAIN=1
+#LOG_HICCUP=1
 #LOG_HICCUP=1
 #LOG_PERFNORM=1
 #LOG_GC=1
 #LOG_JITWATCH=1
-LOG_PERFASM=1
-LOG_JFR=1
+#LOG_PERFASM=1
+#LOG_JFR=1
+LOG_ASYNC=1; ASYNC_EVENT=cpu
+#LOG_ASYNC=1; ASYNC_EVENT=alloc
 
 INFINISPAN_PREBUILT_VERSIONS=""
-#INFINISPAN_PREBUILT_VERSIONS="8.3.0.Final-redhat-1"
-#INFINISPAN_PREBUILT_VERSIONS="8.3.0.Final-redhat-1 8.4.0.ER5-redhat-1"
-#INFINISPAN_PREBUILT_VERSIONS="6.4.2.DR1-redhat-1 6.4.1.Final-redhat-1"
+#INFINISPAN_PREBUILT_VERSIONS="8.4.2.Final-redhat-1 9.4.21.Final 11.0.9.Final"
 
 WORK_DIR=$HOME/Work
 INFINISPAN_HOME=$WORK_DIR/infinispan
 #INFINISPAN_HOME=$WORK_DIR/jdg
 
 INFINISPAN_COMMITS=""
-INFINISPAN_COMMITS="ISPN-wrapped_bytes_hash"
-#INFINISPAN_COMMITS="master ISPN-8410_off-heap_crash ISPN-wrapped_bytes_hash ISPN-off-heap-disabled"
-#INFINISPAN_COMMITS="JDG_7.0.0.GA JDG_7.1.0.ER1 jdg-7.1.x"
-#INFINISPAN_COMMITS="JDG-685/JDG-670/no-bundler/replicated_reads"
+INFINISPAN_COMMITS="main"
 
 FORCE_JGROUPS_VERSION=""
-#FORCE_JGROUPS_VERSION="3.6.10.Final"
+#FORCE_JGROUPS_VERSION="4.0.21.Final"
+#FORCE_JGROUPS_VERSION="4.2.12.Final"
 
-INFINISPAN_CONFIG="../config/infinispan-sync9.xml"
-#INFINISPAN_CONFIG="../config/infinispan-synctx.xml"
-#INFINISPAN_CONFIG="../config/infinispan6-synctx.xml"
+INFINISPAN_CONFIG="../config/infinispan-synctx.xml"; RUN_CONFIG="s"
 
-JGROUPS_CONFIG="default-configs/default-jgroups-tcp.xml"
-#JGROUPS_CONFIG="../config/jgroups-tcp-ufc.xml"
 #JGROUPS_CONFIG="default-configs/default-jgroups-tcp.xml"
-#JGROUPS_CONFIG="default-configs/default-jgroups-udp.xml"
-#JGROUPS_CONFIG="../config/jdg662_modified_udp.xml"
+JGROUPS_CONFIG="default-configs/default-jgroups-udp.xml"
+#JGROUPS_CONFIG="../config/udp-transfer-queue-94.xml"; RUN_CONFIG="$RUN_CONFIG-tq"
 
-COMMON_OPTS="-XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -Xmx4g -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -Xloggc:gc.log -Djava.net.preferIPv4Stack=true -Dorg.jboss.logging.provider=log4j2"
-COMMON_OPTS="$COMMON_OPTS -Dlog4j.configurationFile=file:///$WORK_DIR/infinispan-microbenchmarks/config/log4j2.xml"
+#TEST_JAVA_HOME=/home/dan/.sdkman/candidates/java/8.0.275.hs-adpt; RUN_CONFIG="$RUN_CONFIG-8"
+TEST_JAVA_HOME=/home/dan/.sdkman/candidates/java/11.0.10.hs-adpt; RUN_CONFIG="$RUN_CONFIG-11"
+
+ASYNC_PROFILER_PATH=/home/dan/Tools/async-profiler/build
+
+COMMON_OPTS="-XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -Xmx4g -XX:+HeapDumpOnOutOfMemoryError -XX:+UseG1GC -Djava.net.preferIPv4Stack=true"
+#COMMON_OPTS="-XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -Xmx4g -XX:+UseG1GC -Djava.net.preferIPv4Stack=true"
+COMMON_OPTS="$COMMON_OPTS -Dlog4j.configurationFile=file:///$WORK_DIR/infinispan-microbenchmarks/config/log4j2.xml -Dorg.jboss.logging.provider=log4j2"
 #COMMON_OPTS="-XX:MaxInlineLevel=20"  #inlining helps a bit the async interceptors
 #THROUGHPUT_OPTS="-javaagent:/$WORK_DIR/jHiccup/jHiccup.jar=\"-d $WARMUP_SECONDS -i 1000 -l hiccup.hlog\" -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime"
-THROUGHPUT_OPTS="-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime"
+#THROUGHPUT_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006"
+THROUGHPUT_OPTS=""
 PERFASM_OPTS="-XX:PrintAssemblyOptions=intel"
 JITWATCH_OPTS="-XX:+TraceClassLoading -XX:+LogCompilation -XX:+PrintInlining -XX:+PrintAssembly -XX:PrintAssemblyOptions=intel"
 JFR_OPTIONS=""
 JFR_PARENT_OPTIONS="-Djmh.jfr.stackdepth=128"
-ALT_JFR_OPTIONS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=stackdepth=128,dumponexitpath=. -XX:StartFlightRecording=settings=../config/allocation_profile.jfc,delay=${WARMUP_SECONDS}s,dumponexit=true"
+ALT_JFR_OPTIONS="-XX:+FlightRecorder -XX:FlightRecorderOptions=stackdepth=128 -XX:StartFlightRecording=settings=profile.jfc,delay=${WARMUP_SECONDS}s,dumponexit=true"
+#ALT_JFR_OPTIONS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=stackdepth=128 -XX:StartFlightRecording=settings=../config/allocation_profile.jfc,delay=${WARMUP_SECONDS}s,dumponexit=true"
 #ALT_JFR_OPTIONS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=stackdepth=128,dumponexitpath=. -XX:StartFlightRecording=settings=../config/exception_profile.jfc,delay=${WARMUP_SECONDS}s,dumponexit=true"
 
-cpupower --cpu all frequency-info | grep "current policy" | grep "and 3.00 GHz" || exit 9
+#cpupower --cpu all frequency-info | grep "current policy" | grep "and 3.00 GHz" || exit 9
+
+MAVEN_SETTINGS=$INFINISPAN_HOME/maven-settings.xml
+MAVEN_REPO=$(cat $MAVEN_SETTINGS | perl -ne 'if (/<localRepository>(.*)<\/localRepository>/) { print "$1\n" }' | sed "s%\${user.home}%$HOME%")
+if [ -z "$MAVEN_REPO" ]; then
+  MAVEN_REPO=~/.m2/repository
+fi
 
 function run_java() {
-#  $JAVA_HOME/bin/java "$@"
-  taskset -c 4-7 $JAVA_HOME/bin/java "$@"
+#  $TEST_JAVA_HOME/bin/java "$@"
+  taskset -c $CORES $TEST_JAVA_HOME/bin/java "$@"
 }
 
 function log_version() {
@@ -95,7 +105,7 @@ function log_version() {
   echo Infinispan $INFINISPAN_VERSION
   echo JGroups $JGROUPS_VERSION
   echo Current infinispan working dir commits:
-  echo "$LAST_COMMITS"
+  echo $LAST_COMMITS
   echo
 }
 
@@ -107,20 +117,22 @@ function exit_on_error() {
 }
 
 function run_build() {
-PARAMS="-t $NUM_THREADS -w 5 -wi $((WARMUP_SECONDS / 5)) -r 5 -i $((TEST_SECONDS / 5)) -bm $BENCHMARK_MODE -tu $BENCHMARK_TIME_UNITS -p infinispanConfig=$INFINISPAN_CONFIG -p jgroupsConfig=$JGROUPS_CONFIG -p clusterSize=$CLUSTER_SIZE -p initialFillRadio=$FILL_RATIO -p numKeys=${NUM_KEYS} -p keySize=${KEY_SIZE} -p valueSize=${VALUE_SIZE}"
+PARAMS="-t $NUM_THREADS -w 5 -wi $((WARMUP_SECONDS / 5)) -r 5 -i $((TEST_SECONDS / 5)) -bm $BENCHMARK_MODE -tu $BENCHMARK_TIME_UNITS -p infinispanConfig=$INFINISPAN_CONFIG -p jgroupsConfig=$JGROUPS_CONFIG -p clusterSize=$CLUSTER_SIZE -p initialFillRatio=$FILL_RATIO -p numKeys=${NUM_KEYS} -p keySize=${KEY_SIZE} -p valueSize=${VALUE_SIZE}"
 
-mvn clean package -Dversion.infinispan=$INFINISPAN_VERSION -Dversion.jgroups=$JGROUPS_VERSION
+mvn -s $MAVEN_SETTINGS clean package -Dversion.infinispan=$INFINISPAN_VERSION -Dversion.jgroups=$JGROUPS_VERSION | tee build.log | tail -20
 exit_on_error mvn
 
-BASENAME=$PREFIX-$(date +%Y%m%d-%H%M)-${SUFFIX//[^a-zA-Z0-9-_.]/_}
+BASENAME=$PREFIX-$(date +%Y%m%d-%H%M)-${RUN_VERSION//[^a-zA-Z0-9-_.]/_}-$RUN_CONFIG
 log_version > $BASENAME.log
 echo Results will be in $BASENAME.log
 
 # straight results
 if [ "$LOG_MAIN" == "1" ] ; then
-  run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS $THROUGHPUT_OPTS" -f 3 $TEST $PARAMS >>$BASENAME.log 2>&1
+  run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS $THROUGHPUT_OPTS" -f 3 "$TEST" $PARAMS >>$BASENAME.log
   log_version >> $BASENAME.log
 fi
+
+# jHiccup results
 if [ "$LOG_HICCUP" == "1" ] ; then
    echo Collecting hiccup logs
    mv gc.log $BASENAME-gc.log
@@ -133,12 +145,13 @@ fi
 # perfnorm output
 if [ "$LOG_PERFNORM" == "1" ] ; then
    echo Collecting perfnorm profiler logs
-   run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS" -f 1 -prof perfnorm $TEST $PARAMS >>$BASENAME-perfnorm-gc.log 2>&1
+   run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS" -f 1 -prof perfnorm $TEST $PARAMS >>$BASENAME-perfnorm-gc.log
 fi
 # gc output
 if [ "$LOG_GC" == "1" ] ; then
+   GC_OPTS="-XX:+IgnoreUnrecognizedVMOptions -XX:+PrintGCDetails -XX:+PrintGCApplicationStoppedTime -Xloggc:gc.log -Xlog:gc*,safepoint:gc.log:time,uptime"
    echo Collecting gc profiler logs
-   run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS" -f 1 -prof gc $TEST $PARAMS >>$BASENAME-perfnorm-gc.log 2>&1
+   run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS" -f 1 -prof gc $TEST $PARAMS >>$BASENAME-perfnorm-gc.log
 fi
 if [ "$LOG_PERFNORM" == "1" -o "$LOG_GC" == "1" ] ; then
   log_version >> $BASENAME-perfnorm-gc.log
@@ -147,7 +160,9 @@ fi
 # perfasm output
 if [ "$LOG_PERFASM" == "1" ] ; then
    echo Collecting perfasm profiler logs
-   run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS $PERFASM_OPTS" -f 1 -prof perfasm:hotThreshold=0.01 $TEST $PARAMS >>$BASENAME-perfasm.log 2>&1
+   LD_LIBRARY_PATH=/home/dan/Work/hotspot/src/share/tools/hsdis/build/linux-amd64/ \
+   run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS $PERFASM_OPTS" -f 1 \
+   -prof perfasm:hotThreshold=0.01 $TEST $PARAMS &>$BASENAME-perfasm.log
    log_version >> $BASENAME-perfasm.log
 fi
 
@@ -156,6 +171,7 @@ if [ "$LOG_JITWATCH" == "1" ] ; then
    echo Collecting jitwatch logs
    run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS $JITWATCH_OPTS" -f 1 $TEST $PARAMS
    mv $(ls -t hotspot_pid*.log | head -1) $BASENAME-jitwatch.log
+   log_version >> $BASENAME-perfasm.log
 fi
 
 # flight recorder + flamegraph output
@@ -165,7 +181,7 @@ if [ "$LOG_JFR" == "1" ] ; then
    run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS $ALT_JFR_OPTIONS" -f 1 $TEST $PARAMS
    JFR_BASENAME=$BASENAME-flightrecorder
    I=0
-   for f in $(ls  hotspot-pid*.jfr) ; do
+   for f in hotspot-pid*.jfr ; do
       I=$((I + 1))
       mv $f $JFR_BASENAME$I.jfr
       ~/Work/jfr-flame-graph/run.sh -f $JFR_BASENAME$I.jfr -o >(sed -r -e 's/([^a-zA-Z]|^)([a-z])[a-z]+\./\1\2./g' -e 's/(\.[a-z])[a-z]+\./\1./g' >$JFR_BASENAME$I.stacks)
@@ -175,27 +191,46 @@ if [ "$LOG_JFR" == "1" ] ; then
    I=
 fi
 
+# async-profiler
+if [ "$LOG_ASYNC" == "1" ] ; then
+  echo Collecting async-profiler info
+  ASYNC_PROFILER_BASENAME=$BASENAME-async-profiler$ASYNC_EVENT
+#  ASYNC_PROFILER_OPTIONS="-agentpath:${ASYNC_PROFILER_PATH}/libasyncProfiler.so=start,file=${ASYNC_PROFILER_BASENAME}.folded,collapsed,event=wall,exclude=epoll_wait,exclude=__pthread*,exclude=*__libc_recv*,exclude=__GI___poll"
+  ASYNC_PROFILER_OPTIONS="-agentpath:${ASYNC_PROFILER_PATH}/libasyncProfiler.so=start,file=${ASYNC_PROFILER_BASENAME}.folded,collapsed,event=$ASYNC_EVENT"
+  run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS $ASYNC_PROFILER_OPTIONS" -f 1 "$TEST" $PARAMS | tee "$ASYNC_PROFILER_BASENAME.log"
+#  ASYNC_PROFILER_PARAMS="libPath=$ASYNC_PROFILER_PATH/libasyncProfiler.so;output=text;event=wall;verbose=true"
+#  run_java -jar target/benchmarks.jar -jvmArgsPrepend "$COMMON_OPTS" -f 1 "$TEST" $PARAMS -prof "async:$ASYNC_PROFILER_PARAMS" | tee "$ASYNC_PROFILER_BASENAME.log"
+  log_version >> "$ASYNC_PROFILER_BASENAME.log"
+  java -cp ${ASYNC_PROFILER_PATH}/converter.jar FlameGraph "${ASYNC_PROFILER_BASENAME}.folded" "${ASYNC_PROFILER_BASENAME}.html"
+fi
+
 echo Results are in $BASENAME.log
 tail -1 $BASENAME.log
 }
 
 # main
 for COMMIT in $INFINISPAN_COMMITS; do
-  SUFFIX=$COMMIT
-  BUILD_DIR=$WORK_DIR/tmpbuild/infinispan-microbenchmarks
+  RUN_VERSION=$COMMIT
+  BUILD_DIR=/tmp/build-infinispan-microbenchmarks
   rm -rf $BUILD_DIR
   git clone -s $INFINISPAN_HOME $BUILD_DIR
   pushd $BUILD_DIR
   git checkout $COMMIT
   mvn clean
-  mvn install -s maven-settings.xml -DskipTests -am -pl core
+  mvn install -DskipTests -am -pl core
   INFINISPAN_VERSION=$(cat core/pom.xml | perl -ne 'if (/<version>(.*)<\/version>/) { print "$1\n" }' | head -1)
   if [ -z $FORCE_JGROUPS_VERSION ]; then
-    JGROUPS_VERSION=$(cat bom/pom.xml | perl -ne 'if (/<version.jgroups>(.*)<\/version.jgroups>/) { print "$1\n" }')
+    if [[ -d build-configuration ]]; then
+      JGROUPS_VERSION=$(cat build-configuration/pom.xml | perl -ne 'if (/<version.jgroups>(.*)<\/version.jgroups>/) { print "$1\n" }')
+    elif [ -d bom ]; then
+      JGROUPS_VERSION=$(cat bom/pom.xml | perl -ne 'if (/<version.jgroups>(.*)<\/version.jgroups>/) { print "$1\n" }')
+    else
+      exit_on_error jgroups_version
+    fi
   else
     JGROUPS_VERSION=$FORCE_JGROUPS_VERSION
   fi
-  LAST_COMMITS="$(git log --pretty=format:"%H %s" -3 | git name-rev --stdin)"
+  LAST_COMMITS=$(git log --pretty=format:"%H %s" -3 | git name-rev --stdin)
   popd
 
   run_build || echo "Failed to run test for $COMMIT"
@@ -203,10 +238,16 @@ for COMMIT in $INFINISPAN_COMMITS; do
 done
 
 for INFINISPAN_VERSION in $INFINISPAN_PREBUILT_VERSIONS; do
-  SUFFIX=$INFINISPAN_VERSION
+  RUN_VERSION=$INFINISPAN_VERSION
   if [ -z $FORCE_JGROUPS_VERSION ]; then
-    mvn compile -Dversion.infinispan=$INFINISPAN_VERSION
-    JGROUPS_VERSION=$(cat ~/.m2/repository/org/infinispan/infinispan-bom/$INFINISPAN_VERSION/infinispan-bom-$INFINISPAN_VERSION.pom | perl -ne 'if (/<version.jgroups>(.*)<\/version.jgroups>/) { print "$1\n" }')
+    mvn -s $MAVEN_SETTINGS compile -Dversion.infinispan=$INFINISPAN_VERSION
+    if [[ -d $MAVEN_REPO/org/infinispan/infinispan-build-configuration-parent/$INFINISPAN_VERSION ]]; then
+      JGROUPS_VERSION=$(cat $MAVEN_REPO/org/infinispan/infinispan-build-configuration-parent/$INFINISPAN_VERSION/infinispan-build-configuration-parent-$INFINISPAN_VERSION.pom | perl -ne 'if (/<version.jgroups>(.*)<\/version.jgroups>/) { print "$1\n" }')
+    elif [ -d $MAVEN_REPO/org/infinispan/infinispan-bom/$INFINISPAN_VERSION ]; then
+      JGROUPS_VERSION=$(cat $MAVEN_REPO/org/infinispan/infinispan-bom/$INFINISPAN_VERSION/infinispan-bom-$INFINISPAN_VERSION.pom | perl -ne 'if (/<version.jgroups>(.*)<\/version.jgroups>/) { print "$1\n" }')
+    else
+      exit_on_error jgroups_version
+    fi
   else
     JGROUPS_VERSION=$FORCE_JGROUPS_VERSION
   fi
